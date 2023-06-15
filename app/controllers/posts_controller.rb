9104a951cb5eab53a44a02df00d210fa8f513419
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-15 02:47:20 -0400

    mvp

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def form
+  end
+
+  def create
+    @post = Post.new(posts_params)
+
+    if @post.save
+      redirect_to exito_path
+    else
+      render :new
+    end
+  end
+
+  def success
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :content, :url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/inicio', to: 'posts#index'
+  get '/nueva_publicacion', to: 'posts#form'
+  get '/exito', to: 'posts#success'
+
+  post 'posts/create', to: "posts#create"
+
+  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
